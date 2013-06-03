10f27694b485b963423f13d9a05bbca41e2fbc1b
Scott Baldwin <scott@wellmatchhealth.com>
2013-06-03 13:42:19 -0500

    Adding views to update user account and set name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable
+  devise :database_authenticatable, :rememberable, :trackable, :registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login, :password, :password_confirmation, :remember_me, :remember_token, :first_name, :last_name, :email
